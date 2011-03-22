8d0d8c2052f924dbd24c246670babc07f1bce8e4
Ilya Grigorik <ilya@igvita.com>
2011-03-21 22:30:38 -0400

    cleanup config

--- config.rb
+++ config.rb
@@ -1,25 +1,3 @@
-# {
-#   "periods": 5,
-#   "length": 5,
-#
-#   "size": 100,
-#   "bits": 10,
-#   "hashes": 7,
-#   "seed": 30,
-#
-#   "namespace": "default"
-# }
-
-#     'namespace' => 'default',
-#
-#     'periods' => 3,
-#     'length'  => 60,
-#
-#     'size'    => 1000,
-#     'bits'    => 10,
-#     'hashes'  => 7,
-#     'seed'    => 30
-
 config['namespace'] = 'default'
 config['periods'] = 3
 config['length']  = 60
