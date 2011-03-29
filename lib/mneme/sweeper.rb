fc825b540d78e7375ad0a9d22586ae615e895d35
Ilya Grigorik <ilya@igvita.com>
2011-03-29 13:52:30 -0400

    sweeper should run inside of a fiber

--- lib/mneme/sweeper.rb
+++ lib/mneme/sweeper.rb
@@ -16,17 +16,19 @@ module Mnemosyne
       end
 
       sweeper = Proc.new do
-        current = epoch_name(@config['namespace'], 0, @config['length'])
-        @logger.info "Sweeping old filters, current epoch: #{current}"
+        Fiber.new do
+          current = epoch_name(@config['namespace'], 0, @config['length'])
+          @logger.info "Sweeping old filters, current epoch: #{current}"
 
-        conn = Redis.new
-        @config['periods'].times do |n|
-          name = epoch_name(@config['namespace'], n + @config['periods'], @config['length'])
+          conn = Redis.new
+          @config['periods'].times do |n|
+            name = epoch_name(@config['namespace'], n + @config['periods'], @config['length'])
 
-          conn.del(name)
-          @logger.info "Removed: #{name}"
-        end
-        conn.client.disconnect
+            conn.del(name)
+            @logger.info "Removed: #{name}"
+          end
+          conn.client.disconnect
+        end.resume
       end
 
       sweeper.call
